8e84264051fe23d20afc514b0c8ea56e4e2d5821
Septian Eko <septianekoo66@gmail.com>
2026-08-24 01:07:50 +0700

    add 4 folder and add gitignore

--- test-setup.py
+++ test-setup.py
@@ -4,4 +4,5 @@ import sys
 print(f"python version: {sys.version.split()[0]}")
 print(f"pandas version: {pd.__version__}")
 
-df = pd.DataFrame({"hari":["senin"], "status":["siap"]})
+df = pd.DataFrame({"hari:": ["senin"], "status:": ["siap"]})
+print(df)
